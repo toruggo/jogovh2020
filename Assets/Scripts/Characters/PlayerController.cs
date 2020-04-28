55f477228c6790e87ca072d9583119ca570cc907
toruggo <vhugo08.couto@gmail.com>
2020-04-27 23:03:35 -0300

    Coin prefab

--- Assets/Scripts/Characters/PlayerController.cs
+++ Assets/Scripts/Characters/PlayerController.cs
@@ -40,7 +40,6 @@ public class PlayerController : TouchableGameObject
             if (Physics.Raycast(ray, out hit, Mathf.Infinity, collectibleLayer))
             {
                 pickupTarget = hit.collider.gameObject.GetComponent<CollectibleGameObject>();
-                Debug.Log("Collectible: " + hit.collider.name);
                 agent.SetDestination(hit.point);
             }
             else
