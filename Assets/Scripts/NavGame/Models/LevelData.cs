00591acb8b2d29a5b958babd59d36ebd935da79d
toruggo <vhugo08.couto@gmail.com>
2020-05-13 20:38:14 -0300

    LevelData

---
+++ Assets/Scripts/NavGame/Models/LevelData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NavGame.Models
+{
+    public class LevelData
+    {
+        public int CoinCount { get; private set; }
+
+        public void AddCoins(int amount)
+        {
+            CoinCount += amount;
+        }
+
+        public void ConsumeCoins(int amount)
+        {
+            CoinCount =- amount;
+        }
+
+        public void ValidateCoinAmount(int amount)
+        {
+            if(CoinCount < amount)
+            {
+                throw new InvalidOperationException("Need " + amount + " coins!");
+            }
+        }
+    }
+}
